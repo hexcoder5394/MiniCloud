01957548363c402be10ad60a14d9de652c9d0f7d
Achintha Prasanna <achinthajayalal001@gmail.com>
2026-05-02 19:38:45 +0530

    Modified the timezone file

--- Bash/tzinfo.sh
+++ Bash/tzinfo.sh
@@ -5,5 +5,5 @@ tzinfo=$(timedatectl | grep "Time zone" | awk '{print $3}')
 if [ "$tzinfo" == "Asia/Colombo" ]; then
         echo "Server in correct timezone, $tzinfo"
 else
-        echo "Server in wrong time zone, $tzinfo"
+        echo "Server in wrong time zone, $host_name, $tzinfo"
 fi

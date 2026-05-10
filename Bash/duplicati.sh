2df91ea33b6167f7fb0fce42d8b21886ab6d78ad
Achintha Prasanna <achinthajayalal001@gmail.com>
2026-05-10 10:41:06 +0530

    modified the duplicati sh

--- Bash/duplicati.sh
+++ Bash/duplicati.sh
@@ -4,5 +4,5 @@ if [ -f /etc/duplicati.conf ]; then
     echo "Duplicati is installed"
 else
     echo "Duplicati is not installed"
-    sudo docker compose -f ../docker/duplicati-compose.yml up -d
+    sudo docker compose -f /home/mcadmin/minicloud/duplicati/duplicati-compose.yml up -d
 fi
